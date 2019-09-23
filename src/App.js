41135a799177a69a4b462635b968019f37e90906
cvjude <jjchinosoviolet@gmail.com>
2019-09-23 10:31:50 +0100

    set up react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <aside className="side-section"></aside>
+      <main className="main-section"></main>
+    </div>
+  );
+}
+
+export default App;
