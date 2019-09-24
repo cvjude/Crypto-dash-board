cc42d04ff902f2aa2140086ca75ae7ce044aef28
cvjude <jjchinosoviolet@gmail.com>
2019-09-24 16:49:08 +0100

    Create the side nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavSection from './components/NavSection';
+import Transfers from './pages/Transfers';
+import Balances from './pages/Balances';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <aside className="side-section"></aside>
-      <main className="main-section"></main>
-    </div>
+    <Router>
+      <div className='App'>
+        <aside className='side-section'>
+          <NavSection />
+        </aside>
+        <main className='main-section'>
+          <Route exact path='/transfers' component={Transfers} />
+          <Route exact path='/balances' component={Balances} />
+        </main>
+      </div>
+    </Router>
   );
 }
 
